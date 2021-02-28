3274d5b430b5e54aefb5ef0826ab018c2b068739
Oliver Christensen <orc13a@live.dk>
2021-02-28 18:29:39 +0100

    class

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,13 @@
 function data() {
     fetchStartup().then(results => {
-        
+        results['branches'].forEach(obj => {
+            obj.getDetails();
+            //obj.display('bc');
+        });
     });
 }
 data();
+
+function getBranch() {
+
+}
